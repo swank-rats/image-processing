cecaf37183a2d89335d2bf1a0863091fd19a0384
sla89 <skullsplitter@die-optimisten.net>
2014-10-09 13:40:28 +0200

    showing image with openCV works

--- src/image-processing.cpp
+++ src/image-processing.cpp
@@ -6,25 +6,23 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <cv.h>
-#include <highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
-  Mat image;
-  image = imread( argv[1], 1 );
+  Mat im = imread(argc == 2 ? argv[1] : "lena.png", 1);
+  if (im.empty())
+  {
+    cout << "Cannot open image!" << endl;
+    return -1;
+  }
 
-  if( argc != 2 || !image.data )
-    {
-      printf( "No image data \n" );
-      return -1;
-    }
-
-  namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
-  imshow( "Display Image", image );
-
+  imshow("image", im);
   waitKey(0);
 
   return 0;
