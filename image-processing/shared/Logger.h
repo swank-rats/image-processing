f5eaf3015c2888dfe0f0606c386058ddaa45c644
sla89 <skullsplitter@die-optimisten.net>
2014-10-16 17:29:30 +0200

    added logger

---
+++ image-processing/shared/Logger.h
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Logger.h
+// Author      : ITM13
+// Version     : 1.0
+// Copyright   : Copyright (c) 2014 Swank Rat, MIT License (MIT)
+// Description : 
+//============================================================================
+#pragma once
+#include <string>
+
+using namespace std;
+
+namespace Logger
+{
+	void addMessage(string msg);
+	void addError(string msg);
+}
+
