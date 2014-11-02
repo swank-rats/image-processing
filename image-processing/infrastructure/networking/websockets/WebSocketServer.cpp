7265d5f15a09a4902949be5eb9298a9b371fb5ea
sla89 <skullsplitter@die-optimisten.net>
2014-11-02 16:09:42 +0100

    added websocket server

---
+++ image-processing/infrastructure/networking/websockets/WebSocketServer.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : WebSocketServer.h
+// Author      : ITM13
+// Version     : 1.0
+// Description : 
+//============================================================================
+#include "WebSocketServer.h"
+
+namespace infrastructure {
+	namespace websocket {
+		WebSocketServer::WebSocketServer()
+		{
+		}
+
+
+		WebSocketServer::~WebSocketServer()
+		{
+		}
+	}
+}
---
+++ image-processing/infrastructure/networking/websockets/WebSocketServer.h
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : WebSocketServer.h
+// Author      : ITM13
+// Version     : 1.0
+// Description : 
+//============================================================================
+#pragma once
+namespace infrastructure {
+	namespace websocket {
+		class WebSocketServer
+		{
+		public:
+			WebSocketServer();
+		};
+	}
+}
