2616d9af68d39a9ef2e7a16cc0534a5e55af72bc
sla89 <skullsplitter@die-optimisten.net>
2014-11-03 21:27:44 +0100

    started with openssl for websockets

--- image-processing/infrastructure/networking/websockets/WebSocketServer.h
+++ image-processing/infrastructure/networking/websockets/WebSocketServer.h
@@ -6,7 +6,7 @@
 //============================================================================
 #pragma once
 #include <Poco\Net\HTTPServer.h>
-#include <Poco\Net\ServerSocket.h>
+#include <Poco\Net\SecureServerSocket.h>
 #include <Poco\ThreadPool.h>
 #include <Poco\Logger.h>
 
@@ -21,7 +21,7 @@ namespace infrastructure {
 
 		private:
 			Poco::Net::HTTPServer* server;
-			Poco::Net::ServerSocket socket;
+			Poco::Net::SecureServerSocket socket;
 			Poco::ThreadPool threadPool;
 
 			static Poco::Logger& logger;
