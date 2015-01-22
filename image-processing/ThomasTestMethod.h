9208a7d05bec50a4f802c760e3c8ea16fedd8454
thomasgaida <thomas.gaida@students.fhv.at>
2015-01-22 09:35:51 +0100

    calibri method

--- image-processing/ThomasTestMethod.h
+++ image-processing/ThomasTestMethod.h
@@ -1606,5 +1606,90 @@ public:
 
 	}
 
+	void CalibartionTest(){
+	
+		int numBoards = 0;
+		int numCornersHor=5;
+		int numCornersVer=5;
+
+		int numSquares = numCornersHor * numCornersVer;
+		Size board_sz = Size(numCornersHor, numCornersVer);
+
+		VideoCapture capture = VideoCapture(0);
+
+		vector<vector<Point3f>> object_points;
+		vector<vector<Point2f>> image_points;
+
+		vector<Point2f> corners;
+		int successes = 0;
+
+		Mat image;
+		Mat gray_image;
+		capture >> image;
+
+		vector<Point3f> obj;
+		for (int j = 0; j<numSquares; j++)
+			obj.push_back(Point3f(j / numCornersHor, j%numCornersHor, 0.0f));
+
+		while (successes<numBoards)
+		{
+			cvtColor(image, gray_image, CV_BGR2GRAY);
+			bool found = findChessboardCorners(image, board_sz, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+
+			if (found)
+			{
+				cornerSubPix(gray_image, corners, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+				drawChessboardCorners(gray_image, board_sz, corners, found);
+			}
+
+			imshow("win1", image);
+			imshow("win2", gray_image);
+
+			capture >> image;
+			int key = waitKey(1);
+
+			if (key == 27)
+
+				
+
+			if (key == ' ' && found != 0)
+			{
+				image_points.push_back(corners);
+				object_points.push_back(obj);
+
+				printf("Snap stored!");
+
+				successes++;
+
+				if (successes >= numBoards)
+					break;
+			}
+		}
+
+		Mat intrinsic = Mat(3, 3, CV_32FC1);
+		Mat distCoeffs;
+		vector<Mat> rvecs;
+		vector<Mat> tvecs;
+
+		intrinsic.ptr<float>(0)[0] = 1;
+		intrinsic.ptr<float>(1)[1] = 1;
+
+
+		calibrateCamera(object_points, image_points, image.size(), intrinsic, distCoeffs, rvecs, tvecs);
+
+
+		Mat imageUndistorted;
+		while (1)
+		{
+			capture >> image;
+			undistort(image, imageUndistorted, intrinsic, distCoeffs);
+
+			imshow("win1", image);
+			imshow("win2", imageUndistorted);
+			waitKey(1);
+		}
+
+	}
+
 };
 #endif
